f83fb186d7bd560e58ece3fe954e45ec913233e8
Jul1enF <j.furic@gmail.com>
2024-07-03 16:38:28 +0200

    backend 1ère version finie

--- routes/cart.js
+++ routes/cart.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var router = express.Router();
 const Cart = require('../models/carts');
+const mongoose = require('mongoose')
 
 router.post('/', (req, res)=>{
     const newCart = new Cart({
@@ -24,4 +25,15 @@ router.get('/', (req, res)=>{
             });
     })
 })
+
+router.delete('/:trip', (req, res)=>{
+    let trip=new mongoose.Types.ObjectId(req.params.trip);
+    const deletetrip = { trip };
+    Cart.deleteOne(deletetrip)
+    .then( carts => {  
+                console.log(carts.deletedCount);
+                res.json({ result:true , deletedCount:carts.deletedCount });
+    });
+});
+
 module.exports = router;
